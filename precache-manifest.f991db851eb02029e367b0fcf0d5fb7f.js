ef6f4556182c0c9f34825724af08e740c7c1a2cb
MiguelLopesCardoso <miguel.lopescardoso@outlook.com>
2022-08-10 22:01:26 +0200

    Updates

---
+++ precache-manifest.f991db851eb02029e367b0fcf0d5fb7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c238b5f4d1490f2ccb4",
+    "url": "/4WeWiOver/static/css/main.5999f947.chunk.css"
+  },
+  {
+    "revision": "2c238b5f4d1490f2ccb4",
+    "url": "/4WeWiOver/static/js/main.2c238b5f.chunk.js"
+  },
+  {
+    "revision": "8e2bdfc119e7a272a29b",
+    "url": "/4WeWiOver/static/css/1.badbb0d8.chunk.css"
+  },
+  {
+    "revision": "8e2bdfc119e7a272a29b",
+    "url": "/4WeWiOver/static/js/1.8e2bdfc1.chunk.js"
+  },
+  {
+    "revision": "83fcf1e7cf2d76a25408",
+    "url": "/4WeWiOver/static/js/runtime~main.83fcf1e7.js"
+  },
+  {
+    "revision": "ceafc2c5a7fe32fb86536258e20f1270",
+    "url": "/4WeWiOver/index.html"
+  }
+];
